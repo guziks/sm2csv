0ae78b19b99f314fa6071ddf845cc20486b9e66e
Serge Guzik <guzik.sergey@gmail.com>
2015-11-24 10:16:58 +0200

    Add support for SoMachine block comments: they are simply omitted

--- app/src/main/java/ua/com/elius/sm2csv/reader/SoMachineReader.java
+++ app/src/main/java/ua/com/elius/sm2csv/reader/SoMachineReader.java
@@ -38,6 +38,8 @@ public class SoMachineReader {
 
     public SoMachineReader read() {
         mRecords = new ArrayList<>();
+        Pattern blockCommentBeginPattern = Pattern.compile("\\(\\*");
+        Pattern blockCommentEndPattern = Pattern.compile("\\*\\)");
         Pattern commentPattern = Pattern.compile("^\\s+//(.+)$");
         Pattern recordPattern = Pattern.compile("^\\s+((?!//).)+$");
         try {
@@ -47,9 +49,18 @@ public class SoMachineReader {
                     if (curExtention.equals(mExtention)) {
                         try {
                             ArrayList<String> curComments = new ArrayList<>();
-                            Files.lines(filePath).forEach(line -> {
+                            boolean inBlockComment = false;
+                            for (String line : Files.readAllLines(filePath)) {
+                                Matcher blockCommentBeginMatcher = blockCommentBeginPattern.matcher(line);
+                                Matcher blockCommentEndMatcher = blockCommentEndPattern.matcher(line);
                                 Matcher commentMatcher = commentPattern.matcher(line);
                                 Matcher recordMatcher = recordPattern.matcher(line);
+                                if (blockCommentBeginMatcher.find()) inBlockComment = true;
+                                if (blockCommentEndMatcher.find()) {
+                                    inBlockComment = false;
+                                    continue;
+                                }
+                                if (inBlockComment) continue;
                                 if (commentMatcher.matches()) {
                                     curComments.add(commentMatcher.group(1).trim());
                                 } else if (recordMatcher.matches()) {
@@ -60,7 +71,7 @@ public class SoMachineReader {
                                     }
                                     mRecords.add(rec);
                                 }
-                            });
+                            }
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
