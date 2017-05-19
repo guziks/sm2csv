a59b7f8ff093fc8bfba0cb88dc22e81742128c1f
Serge Guzik <guzik.sergey@gmail.com>
2017-05-19 23:26:10 +0300

    refactor: Add abstract CSVWriter

---
+++ app/src/main/java/ua/com/elius/sm2csv/writer/CSVWriter.java
@@ -0,0 +1,80 @@
+package ua.com.elius.sm2csv.writer;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.*;
+import java.util.List;
+
+public abstract class CSVWriter {
+
+    private CSVPrinter mPrinter;
+    private CSVFormat mFormat;
+    private boolean mOpened;
+    private String mOutputFileName;
+    private String mOutputFileEncoding;
+    private String mTargetSoftwareName; // e.g. EasyBuilder
+    private String mTargetTypeName; // e.g. alarm, tag
+
+    public CSVWriter(CSVFormat format, String outputFileName, String outputFileEncoding,
+                     String targetSoftwareName, String targetTypeName) {
+        mFormat = format;
+        mOutputFileName = outputFileName;
+        mOutputFileEncoding = outputFileEncoding;
+        mTargetSoftwareName = targetSoftwareName;
+        mTargetTypeName = targetTypeName;
+
+        open();
+    }
+
+    public boolean isOpened() {
+        return mOpened;
+    }
+
+    private void open(){
+        OutputStreamWriter writer = null;
+
+        try {
+            writer = new OutputStreamWriter(
+                    new FileOutputStream(mOutputFileName), mOutputFileEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            System.out.println(mOutputFileName + " not found");
+        }
+
+        if (writer == null) {
+            return;
+        }
+
+        try {
+            mPrinter = new CSVPrinter(writer, mFormat);
+            mOpened = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (mOpened) {
+            onOpen();
+        }
+    }
+
+    abstract void onOpen();
+
+    void write(List<String> record) {
+        try {
+            mPrinter.printRecord(record);
+        } catch (IOException e) {
+            System.out.println("Failed to write " + mTargetSoftwareName + " " + mTargetTypeName + " record");
+        }
+    }
+
+    public void close() {
+        try {
+            mPrinter.close();
+            mOpened = false;
+        } catch (IOException e) {
+            System.out.println("Failed to close " + mTargetSoftwareName + " " + mTargetTypeName + " file");
+        }
+    }
+}
