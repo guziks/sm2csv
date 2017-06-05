d346b832862709d80dcbf866d15b1bf5957f7d02
Serge Guzik <guzik.sergey@gmail.com>
2017-06-05 15:10:10 +0300

    refactor: Rename TestSoMachineRecord -> SoMachineRecordTest

--- app/src/test/java/SoMachineRecordTest.java
+++ app/src/test/java/SoMachineRecordTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-public class TestSoMachineRecord {
+public class SoMachineRecordTest {
 
     @Test
     public void testFromString() {
