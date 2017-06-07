dd882db52f63b804d7daed1704bc2bef0a9fa938
Serge Guzik <guzik.sergey@gmail.com>
2017-06-07 16:04:50 +0300

    docs: Add MainTest javadoc comments

--- app/src/test/java/MainTest.java
+++ app/src/test/java/MainTest.java
@@ -13,12 +13,27 @@ import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
+/**
+ * Functional test on samples
+ */
 public class MainTest {
 
     private static final Path TEST_PATH = Paths.get("test");
+
+    /**
+     * Subdirectory that stores test samples
+     */
     private static final Path SAMPLES_PATH = TEST_PATH.resolve("samples");
+
+    /**
+     * Subdirectory to copy to for actual testing
+     */
     private static final Path TEST_WORK_PATH = TEST_PATH.resolve("work");
 
+    /**
+     * Returns samples subdirectories
+     * @return stream of paths to cases
+     */
     private static Stream<Path> getCasesPaths () {
         Stream<Path> paths = null;
         try {
@@ -30,6 +45,10 @@ public class MainTest {
         return paths;
     }
 
+    /**
+     * Copies case input files to corresponding case working directory
+     * @param casePath Path to copy from
+     */
     private static void copyCase(Path casePath) {
         File src = casePath.resolve("in").toFile();
         File dst = TEST_WORK_PATH.resolve(casePath.getFileName()).toFile();
@@ -40,6 +59,10 @@ public class MainTest {
         }
     }
 
+    /**
+     * Deletes case from working directory
+     * @param casePath Case path inside samples
+     */
     private static void deleteCase(Path casePath) {
         File caseDir = TEST_WORK_PATH.resolve(casePath.getFileName()).toFile();
         try {
@@ -49,6 +72,9 @@ public class MainTest {
         }
     }
 
+    /**
+     * Deletes whole working directory @{TEST_WORK_PATH}
+     */
     private static void deleteAllCases() {
         try {
             FileUtils.deleteDirectory(TEST_WORK_PATH.toFile());
@@ -57,14 +83,27 @@ public class MainTest {
         }
     }
 
+    /**
+     * Returns case path inside working directory
+     * @param casePath Case path inside samples
+     * @return Case path inside working directory @{link TEST_WORK_PATH}
+     */
     private static Path getCaseWorkPath(Path casePath) {
         return TEST_WORK_PATH.resolve(casePath.getFileName()).toAbsolutePath();
     }
 
+    /**
+     * Returns path to directory containing expected output
+     * @param casePath Case path inside samples
+     * @return Path to directory containing expected output
+     */
     private static Path getCaseOutPath(Path casePath) {
         return casePath.resolve("out");
     }
 
+    /**
+     * Checks whether samples directory correct
+     */
     @BeforeClass
     public static void haveSamples() {
         assertTrue("Samples folder exists", Files.isDirectory(SAMPLES_PATH));
@@ -73,11 +112,17 @@ public class MainTest {
         assertEquals("case1", case1);
     }
 
+    /**
+     * Deletes tests working directory @{link TEST_WORK_PATH}
+     */
     @AfterClass
     public static void clean() {
         deleteAllCases();
     }
 
+    /**
+     * Checks on samples whether converted files equals to expected
+     */
     @Test
     public void testOnSamples() {
         getCasesPaths().forEach(casePath -> {
